2d7697bfd59806d8b79433926b97b74ad7f53e4e
Guy Liba <guy33liba@gmail.com>
2024-11-13 14:42:15 +0200

    next js

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import Link from "next/link"
+import React from "react"
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      title: "Home",
+      url: "/",
+    },
+    {
+      id: 2,
+      title: "Portfolio",
+      url: "/portfolio",
+    },
+    {
+      id: 3,
+      title: "Blog",
+      url: "/blog",
+    },
+    {
+      id: 4,
+      title: "About",
+      url: "/about",
+    },
+    {
+      id: 5,
+      title: "Contact",
+      url: "/contact",
+    },
+    {
+      id: 6,
+      title: "Dashboard",
+      url: "/dashboard",
+    },
+  ]
+
+  return (
+    <Link href="/">
+      {links.map((link) => {
+        return (
+          <Link key={link.id} href={link.url}>
+            <span>{link.title}</span>
+          </Link>
+        )
+      })}
+    </Link>
+  )
+}
+
+export default Navbar
